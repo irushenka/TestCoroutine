ff97d3cb0fb5381a74442a27bf89d9448be11187
irushenka <sindirina@mail.ru>
2023-01-21 14:53:05 +0300

    Initial commit

---
+++ app/src/main/kotlin/com/example/testcoroutine/Post.kt
@@ -0,0 +1,39 @@
+package com.example.testcoroutine
+
+data class Post(
+    val id: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+    var attachment: Attachment? = null,
+)
+
+data class Attachment(
+    val url: String,
+    val description: String,
+    val type: String,
+)
+
+data class Comment(
+    val id: Long,
+    val postId: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+)
+
+data class Author(
+    val id: Long,
+    val name: String,
+    val avatar: String,
+)
+
+data class PostWithAuthorAndComments(
+    val post: Post,
+    val author: Author,
+    val comments: List<Comment>,
+)
